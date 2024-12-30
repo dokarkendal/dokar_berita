2662879646ba6fd79245622de69258dc008f2073
Farid Istiqlal <faridistiqlal9@gmail.com>
2024-12-30 09:53:06 +0700

    Update .gitignore to exclude sensitive files

--- lib/config/api_config.dart
+++
@@ -1,8 +0,0 @@
-class ApiConfig {
-  // Base URL untuk service
-  static const String baseUrl = 'http://dokar.kendalkab.go.id';
-
-  // Headers yang digunakan
-  static Map<String, String> get headers =>
-      {"Key": "VmZNRWVGTjhFeVptSUFJcjdURDlaQT09"};
-}
